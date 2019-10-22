2e3f07c9ffe87d6b7fb7c75b3cc1f19cf15ca1a7
William Chrapcynski <wchrapcynski@gmail.com>
2019-10-22 14:51:56 -0400

    Created 5 questions to start with

--- html/js/main.js
+++ html/js/main.js
@@ -1,8 +1,12 @@
+// DOM variables
 const questionBox = document.querySelector('questionBox');
-const start = document.querySelector('start');
-const quite = document.querySelector('quit');
+const menu1 = document.querySelector('menu1');
+const menu2 = document.querySelector('menu2');
 const answer1 = document.querySelector('answer1');
 const answer2 = document.querySelector('answer2');
 const answer3 = document.querySelector('answer3');
 const answer4 = document.querySelector('answer4');
 const infobox = document.querySelector('infobox');
+
+// Boolean variables
+let gameOver = true;
---
+++ html/js/questions.js
@@ -0,0 +1,27 @@
+const questions = [
+    {
+        question: "In The Sword in the Stone, what does Merlin call The Greatest Force on Earth?",
+        answer: "Love",
+        choices: ["Love", "Magic", "Arthur", "Water"]
+    },
+    {
+        question: "In Aladdin, what is the name of Jasmine’s pet tiger?",
+        answer: "Rajah",
+        choices: ["Rajah", "Tigey", "Zara", "Kintaro"]
+    },
+    {
+        question: "How did Walt’s Brother Roy propose to his wife Edna?",
+        answer: "By telegram",
+        choices: ["By telegram", "By phone", "In person", "By mail"]
+    },
+    {
+        question: "What author wrote the book that the animated feature The Jungle Book is based on?",
+        answer: "Rudyard Kipling",
+        choices: ["Rudyard Kipling", "L. Frank Baum", "C. S. Lewis", "Roald Dahl"]
+    }
+    {
+        question: "When does Mary Poppins say she will leave the Banks’ house?",
+        answer: "When the wind changes",
+        choices: ["When the wind changes", "When the story is over", "When the children grow up", "When the money runs out"]
+    }
+]
