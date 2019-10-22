d555a460eb987b9cfb9f2b9d8abbe5a8224ab74a
William Chrapcynski <wchrapcynski@gmail.com>
2019-10-22 16:42:24 -0400

    Most of game logic is in

--- html/js/main.js
+++ html/js/main.js
@@ -23,7 +23,8 @@ let questionAnswered = false;
 
 // Variables
 let currentQuestionNum = 1;
-let maxQuestions = 20;
+let maxQuestions = questions.length;
+let score = 0;
 
 // Fisher-Yates Shuffle
 function shuffle(array) {
@@ -51,13 +52,38 @@ function grabQuestion() {
     for(let i = 0; i < 4; i++) {
         answerText[i].innerText = questions[currentQuestionNum - 1].choices[i];
     }
-    currentQuestionNum += 1;
+    menu1.classList.add("greyedOut")
+    menu1.removeEventListener('click', startGame);
 }
 
 function startGame () {
     gameOver = false;
-    menu1.removeEventListener('click', startGame);
     menu1.innerText = "Next Question";
     grabQuestion();
+    choicesActivate();
+}
+
+function choicesActivate() {
+    for(let i = 0; i < 4; i++) {
+        answerText[i].addEventListener('click', checkAnswer);
+    }
+}
+
+function checkAnswer() {
+    console.log(this.innerText)
+    console.log(questions[currentQuestionNum - 1].answer)
+    if (this.innerText === questions[currentQuestionNum - 1].answer) {
+        console.log("This is correct!")
+        score += 1;
+        playerScore.innerText = score;
+    } else {
+        console.log("This is not correct!")
+    }
+    if (currentQuestionNum === questions.length) {
+        console.log("The game is over!");
+        return 0;
+    }
+    currentQuestionNum += 1;
+    menu1.classList.remove("greyedOut")
     menu1.addEventListener('click', grabQuestion)
 }
